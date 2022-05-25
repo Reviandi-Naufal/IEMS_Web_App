29ecb1c5a8ad889f881197a9c487a6ce4f8b71f5
Reviandi Naufal <reviandinaufalk@gmail.com>
2022-05-25 22:04:35 +0700

    Add database for login & signin, make account file

---
+++ FLask_Blog/Blog/database.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from Blog import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    #setting __repr__ method, it's to define how the object is printed out whenever it get printed out
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}','{self.image_file}')"
