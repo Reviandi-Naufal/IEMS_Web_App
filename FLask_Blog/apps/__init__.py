d838b6cc5597c9a9f6cdbf36cb8f0e43b0852133
fajrinurf <fajrinurfauzan@student.telkomuniversity.ac.id>
2022-07-07 17:07:25 +0700

    update

--- FLask_Blog/apps/__init__.py
+++ FLask_Blog/apps/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.secret_key = "geekscoderssecretkey"
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Ismilelabn209!!@172.23.142.168:3306/db_ismile' # Connect to Database Server
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:MilitenSire360@localhost:3306/db_ismile' # Connect to Database Server
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:root@localhost:3306/i-smile' # Connect to Database Server
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
