28010b3335e6df15799973d67be5343252c7b0cc
fajrinurf <fajrinurfauzan@student.telkomuniversity.ac.id>
2022-07-28 19:57:56 +0700

    up

--- FLask_Blog/apps/static/rnnlinechart.js
+++ FLask_Blog/apps/static/rnnlinechart.js
@@ -1,44 +1,43 @@
 $.ajax({
-    type: 'GET',
-    url: 'https://iems-telu.ismailab.xyz/get_data_rnnlineChart',
-    success: function (response) {
-      var objectData = response;
-      console.log(objectData, typeof objectData);
-      // var select = Object.values(response)
-      var dataSumbuX = objectData.datetime;
-      var dataSumbuY = objectData.Kwh;
-      // var dataCoba = Object.values(select)
-      // console.log(dataSumbuY)
-      buatRNNLineChart(dataSumbuX, dataSumbuY);
+  type: 'GET',
+  url: 'https://iems-telu.ismailab.xyz/get_data_rnnlineChart',
+  success: function (response) {
+    var objectData = response;
+    console.log(objectData, typeof objectData);
+    // var select = Object.values(response)
+    var dataSumbuX = objectData.datetime;
+    var dataSumbuY = objectData.Kwh;
+    // var dataCoba = Object.values(select)
+    // console.log(dataSumbuY)
+    buatRNNLineChart(dataSumbuX, dataSumbuY);
+  },
+});
+
+function buatRNNLineChart(dataSumbuX, dataSumbuY) {
+  var lineChart = echarts.init(document.getElementById('rnnlineChart'));
+
+  var option = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow',
+      },
     },
-  });
-  
-  function buatRNNLineChart(dataSumbuX, dataSumbuY) {
-    var lineChart = echarts.init(document.getElementById('rnnlineChart'));
-  
-    var option = {
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'shadow',
-        },
+    xAxis: {
+      type: 'category',
+      data: dataSumbuX,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: dataSumbuY,
+        type: 'line',
       },
-      xAxis: {
-        type: 'category',
-        data: dataSumbuX,
-      },
-      yAxis: {
-        type: 'value',
-      },
-      series: [
-        {
-          data: dataSumbuY,
-          type: 'line',
-        },
-      ],
-      responsive: true,
-    };
-  
-    lineChart.setOption(option);
-  }
-  
+    ],
+    responsive: true,
+  };
+
+  lineChart.setOption(option);
+}
