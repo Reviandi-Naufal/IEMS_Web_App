35469a246348a0cc6788c71b1ac01f0b1b6a4eec
fajrinurf <fajrinurfauzan@student.telkomuniversity.ac.id>
2022-07-29 17:01:13 +0700

    Update tcnlinecharts.js

--- FLask_Blog/apps/static/tcnlinecharts.js
+++ FLask_Blog/apps/static/tcnlinecharts.js
@@ -17,25 +17,106 @@ function buatTCNLineChart(dataSumbuX, dataSumbuY) {
   var lineChart = echarts.init(document.getElementById('tcnlineChart'));
 
   var option = {
+    title: {
+      text: 'Predictions Chart',
+      left: '1%',
+    },
     tooltip: {
       trigger: 'axis',
-      axisPointer: {
-        type: 'shadow',
-      },
+    },
+    grid: {
+      left: '5%',
+      right: '15%',
+      bottom: '10%',
     },
     xAxis: {
-      type: 'category',
       data: dataSumbuX,
     },
-    yAxis: {
-      type: 'value',
+    yAxis: {},
+    toolbox: {
+      right: 10,
+      feature: {
+        dataZoom: {
+          yAxisIndex: 'none',
+        },
+        restore: {},
+        saveAsImage: {},
+      },
     },
-    series: [
+    dataZoom: [
       {
-        data: dataSumbuY,
-        type: 'line',
+        startValue: '2021-09-01',
+      },
+      {
+        type: 'inside',
       },
     ],
+    visualMap: {
+      top: 50,
+      right: 10,
+      pieces: [
+        {
+          gt: 0,
+          lte: 5,
+          color: '#93CE07',
+        },
+        {
+          gt: 5,
+          lte: 10,
+          color: '#FFF33B',
+        },
+        {
+          gt: 10,
+          lte: 15,
+          color: '#FDC70C',
+        },
+        {
+          gt: 15,
+          lte: 20,
+          color: '#F3903F',
+        },
+        {
+          gt: 20,
+          lte: 25,
+          color: '#ED683C',
+        },
+        {
+          gt: 25,
+          color: '#C93C3A',
+        },
+      ],
+      outOfRange: {
+        color: '#999',
+      },
+    },
+    series: {
+      name: 'Predictions Chart',
+      type: 'line',
+      data: dataSumbuY,
+      markLine: {
+        silent: true,
+        lineStyle: {
+          color: '#799',
+        },
+        data: [
+          {
+            yAxis: 5,
+          },
+          {
+            yAxis: 10,
+          },
+          {
+            yAxis: 15,
+          },
+          {
+            yAxis: 20,
+          },
+          {
+            yAxis: 25,
+          },
+        ],
+      },
+    },
     responsive: true,
   };
 
