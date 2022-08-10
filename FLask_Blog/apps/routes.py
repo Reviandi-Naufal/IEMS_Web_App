9836ce4ee7ed79ac0ca33d2eb05e1dd17014ec99
Reviandi Naufal <reviandinaufalk@gmail.com>
2022-08-10 16:57:50 +0700

    Fixing clustering datatable 2

--- FLask_Blog/apps/routes.py
+++ FLask_Blog/apps/routes.py
@@ -982,7 +982,7 @@ def clusterdata():
             break
         col_name = request.args.get(f'columns[{col_index}][data]')
         if col_name not in ['DateTime', 'Kwh','Date', 'Time', 'old_kwh', 'delta_kwh', 'kluster', 'Category']:
-            col_name = 'DateTime'
+            col_name = 'Date'
         descending = request.args.get(f'order[{i}][dir]') == 'desc'
         col = getattr(Klastering_Perbulan_DataReal, col_name)
         if descending:
